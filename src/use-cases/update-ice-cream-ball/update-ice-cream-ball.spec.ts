1834948f677d35008f79deff2dfec0622cccb6b6
Cícero Mello <ciceromello.dev@gmail.com>
2025-03-01 16:29:28 -0300

    📝 ✅ Testing UpdateIceCreamBall

---
+++ src/use-cases/update-ice-cream-ball/update-ice-cream-ball.spec.ts
@@ -0,0 +1,106 @@
+import { describe, expect, test } from "vitest"
+import * as R from "#repositories/in-memory"
+import { CreateCustomer } from "#use-cases/create-customer"
+import { Avatar, Size } from "#enums"
+import { CreateIceCream } from "#use-cases/create-ice-cream"
+import { BallFlavor, IceCream, IceCreamBall, IceCreamCup } from "#entities"
+import { UpdateIceCreamBall } from "."
+import { getError } from "#utils"
+import { nanoid } from "nanoid"
+
+describe("Use Cases || UpdateIceCreamBall", () => {
+    test("Updating With Valid/Invalid IceCreamId", async () => {
+        const iceCreamConeRepo = new R.IceCreamConeRepoInMemory()
+
+        const iceCreamCupRepo = new R.IceCreamCupRepoInMemory()
+        const iceCreamBallRepo = new R.IceCreamBallRepoInMemory()
+        const customerRepo = new R.CustomerRepoInMemory()
+        const iceCreamRepo = new R.IceCreamRepoInMemory({
+            iceCreamBallRepo,
+            iceCreamConeRepo,
+            iceCreamCupRepo
+        })
+
+        const createCustomer = new CreateCustomer({ customerRepo })
+        const { customer } = await createCustomer.execute({
+            avatar: Avatar.YoungMan,
+            name: "Johnny",
+            pass: "veryNicePassword"
+        })
+
+        const createIceCream = new CreateIceCream({
+            iceCreamBallRepo,
+            iceCreamConeRepo,
+            iceCreamCupRepo,
+            iceCreamRepo,
+            customerRepo
+        })
+
+        const ball = new IceCreamBall({
+            flavor: BallFlavor.chocolate,
+            size: Size.small,
+        })
+
+        const iceCream = new IceCream({
+            name: "Just a simple name",
+            base: new IceCreamCup({ size: Size.medium }),
+            balls: [ball]
+        })
+
+        await createIceCream.execute({
+            customerId: customer.id,
+            iceCream: iceCream
+        })
+
+        expect(iceCreamBallRepo.iceCreamBalls).toHaveLength(1)
+        expect(iceCreamBallRepo.iceCreamBalls[0].id).toEqual(ball.id)
+        expect(iceCreamBallRepo.iceCreamBalls[0].flavor).toEqual(ball.flavor)
+        expect(iceCreamBallRepo.iceCreamBalls[0].size).toEqual(ball.size)
+        expect(iceCreamBallRepo.iceCreamBalls[0].iceCreamId).toEqual(iceCream.id)
+
+        // Valid -----------------------------------------------------------------------
+        try {
+            const updateIceCreamBall = new UpdateIceCreamBall({
+                iceCreamBallRepo,
+                iceCreamRepo
+            })
+            ball.updateBall({
+                flavor: BallFlavor.vanilla,
+                size: Size.big
+            })
+            await updateIceCreamBall.execute({
+                iceCreamBall: ball,
+                iceCreamId: iceCream.id
+            })
+            expect(iceCreamBallRepo.iceCreamBalls).toHaveLength(1)
+            expect(iceCreamBallRepo.iceCreamBalls[0].id).toEqual(ball.id)
+            expect(iceCreamBallRepo.iceCreamBalls[0].iceCreamId).toEqual(iceCream.id)
+            expect(iceCreamBallRepo.iceCreamBalls[0].flavor).toEqual(BallFlavor.vanilla)
+            expect(iceCreamBallRepo.iceCreamBalls[0].size).toEqual(Size.big)
+        } catch (error) {
+            expect.fail(getError(error))
+        }
+        // -----------------------------------------------------------------------------
+
+        // Invalid ---------------------------------------------------------------------
+        try {
+            const updateIceCreamBall = new UpdateIceCreamBall({
+                iceCreamBallRepo,
+                iceCreamRepo
+            })
+            ball.updateBall({
+                flavor: BallFlavor.vanilla,
+                size: Size.big
+            })
+            await updateIceCreamBall.execute({
+                iceCreamBall: ball,
+                iceCreamId: nanoid()
+            })
+            expect.fail("Didn't Trigger Error")
+        } catch (error) {
+            expect(!!error).toEqual(true)
+            return
+        }
+        // -----------------------------------------------------------------------------
+    })
+})
