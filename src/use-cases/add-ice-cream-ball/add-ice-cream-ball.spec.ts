c956fa3d655469f36d58acade8aaec1942f23a2f
Cícero Mello <ciceromello.dev@gmail.com>
2025-03-01 15:56:15 -0300

    📝 ✅ Testing AddIceCreamBall

---
+++ src/use-cases/add-ice-cream-ball/add-ice-cream-ball.spec.ts
@@ -0,0 +1,89 @@
+import { describe, expect, test } from "vitest"
+import * as R from "#repositories/in-memory"
+import { CreateCustomer, CreateIceCream } from "#use-cases"
+import { Avatar, Size } from "#enums"
+import { BallFlavor, IceCream, IceCreamBall, IceCreamCup } from "#entities"
+import { AddIceCreamBall } from "."
+import { getError } from "#utils"
+import { nanoid } from "nanoid"
+
+describe("Use Cases || AddIceCreamBall", () => {
+    test("Adding With Valid/Invalid IceCreamId", async () => {
+ const iceCreamConeRepo = new R.IceCreamConeRepoInMemory()
+        const iceCreamCupRepo = new R.IceCreamCupRepoInMemory()
+        const iceCreamBallRepo = new R.IceCreamBallRepoInMemory()
+        const customerRepo = new R.CustomerRepoInMemory()
+        const iceCreamRepo = new R.IceCreamRepoInMemory({
+            iceCreamBallRepo,
+            iceCreamConeRepo,
+            iceCreamCupRepo
+        })
+
+        const createCustomer = new CreateCustomer({ customerRepo })
+        const { customer } = await createCustomer.execute({
+            avatar: Avatar.YoungMan,
+            name: "Johnny",
+            pass: "veryNicePassword"
+        })
+
+        const createIceCream = new CreateIceCream({
+            iceCreamBallRepo,
+            iceCreamConeRepo,
+            iceCreamCupRepo,
+            iceCreamRepo,
+            customerRepo
+        })
+
+        const iceCream = new IceCream({
+            name: "Just a simple name",
+            base: new IceCreamCup({ size: Size.medium }),
+            balls: []
+        })
+
+        await createIceCream.execute({
+            customerId: customer.id,
+            iceCream: iceCream
+        })
+
+        const newBall = new IceCreamBall({
+            flavor: BallFlavor.chocolate,
+            size: Size.small,
+        })
+
+        expect(iceCreamBallRepo.iceCreamBalls).toHaveLength(0)
+
+        // Valid -------------------------------------------------------------
+        try {
+            const addIceCreamBall = new AddIceCreamBall({
+                iceCreamBallRepo,
+                iceCreamRepo
+            })
+            await addIceCreamBall.execute({
+                iceCreamBall: newBall,
+                iceCreamId: iceCream.id
+            })
+            expect(iceCreamBallRepo.iceCreamBalls).toHaveLength(1)
+            expect(iceCreamBallRepo.iceCreamBalls[0].id).toEqual(newBall.id)
+        } catch (error) {
+            expect.fail(getError(error))
+        }
+        // --------------------------------------------------------------------
+
+        // Invalid ------------------------------------------------------------
+        try {
+            const addIceCreamBall = new AddIceCreamBall({
+                iceCreamBallRepo,
+                iceCreamRepo
+            })
+            await addIceCreamBall.execute({
+                iceCreamBall: newBall,
+                iceCreamId: nanoid()
+            })
+        } catch (error) {
+            expect(!!error).toEqual(true)
+            return
+        }
+        expect.fail("Didn't Trigger Error")
+        // --------------------------------------------------------------------
+    })
+})
--- src/use-cases/index.ts
+++ src/use-cases/index.ts
@@ -12,3 +12,9 @@ export * from "./get-customer-data/types"
 
 export * from "./get-customer-ice-creams"
 export * from "./get-customer-ice-creams/types"
+
+export * from "./rename-ice-cream"
+export * from "./rename-ice-cream/types"
+
+export * from "./add-ice-cream-ball"
+export * from "./add-ice-cream-ball/types"
