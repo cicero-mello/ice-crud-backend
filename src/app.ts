d733063dd4068938849d5c09be23ea088f3cc3f1
Cícero Mello <ciceromello.dev@gmail.com>
2025-03-15 11:03:13 -0300

    🔒️ Registering CORS

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,27 @@
 import Fastify from "fastify"
 import { registerRoutes } from "#routes"
 import { setupSwagger } from "#libs/swagger"
+import cors from "@fastify/cors"
 
 const fastify = Fastify()
+
+fastify.register(cors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+})
 
 const start = async () => {
     try {
         console.log("⏳ Loading...")
         await setupSwagger(fastify)
         registerRoutes(fastify)
-        await fastify.listen({ port: 3000 })
+        await fastify.listen({ port: 8080 })
 
         console.clear()
-        console.log('🍦 API Running in: http://localhost:3000')
-        console.log('📄 Docs Running in: http://localhost:3000/docs')
+        console.log('🍦 API Running in: http://localhost:8080')
+        console.log('📄 Docs Running in: http://localhost:8080/docs')
     } catch (err) {
         fastify.log.error(err)
         process.exit(1)
